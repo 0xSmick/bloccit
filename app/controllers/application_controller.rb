0d45a490516ca9611bbce4aae364e056d0e8b251
undeadsushi <sheldon.smickley@gmail.com>
2014-03-15 13:55:34 -0400

    Associated users and posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+ def create
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+  
 end
