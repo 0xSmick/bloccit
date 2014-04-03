a51ea2b5eedcb5a30de318f64504bd444ecf2d2b
undeadsushi <sheldon.smickley@gmail.com>
2014-04-03 08:52:51 -0400

    added destroy functionality

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -8,4 +8,10 @@ class CommentPolicy < ApplicationPolicy
     user.present?
   end
 
+  def destroy?
+    user.present? && (record.user == user || user.role?(:admin) || user.role?(:moderator))
+    end
 end
+
+
+
