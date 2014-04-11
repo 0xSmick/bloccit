258a3b4e2bdf2019852d65cab74efbf952b9cc4e
undeadsushi <sheldon.smickley@gmail.com>
2014-04-10 23:42:16 -0400

    Created popular posts and users, and optimized topics#show with eager laoding.

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,19 @@ class User < ActiveRecord::Base
     self.votes.where(post_id: post.id).first
   end
   
+  def self.top_rated
+    self.select('users.*'). # Select all attributes of the user
+    select('COUNT(DISTINCT comments.id) AS comments_count'). # count the posts made by the user
+    select('COUNT(DISTINCT posts.id) AS posts_count'). # Count the posts made by the user
+    select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank'). # Add the comment count to the post count and label the sum as "rank"
+    joins(:posts). # ties the posts table to the users table, via the user_id
+    joins(:comments). # ties the comments table to the user table, via the user_id
+    group('users.id'). #Instructs the database to group the results so that each user will be returned in a distinct row
+    order('rank DESC') #Instructs the database to order the results in descending order, by the rank that we created in this query. (rank = comment count + post count)
+
+  end
+
+  ROLES = %w[member moderator admin]
 
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
   devise_for :users
-  resources :users, only: [:show, :update]
-
+  resources :users, only: [:show, :index, :update]
+  resources :posts, only: [:index]
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
