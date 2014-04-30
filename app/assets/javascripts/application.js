d629a533637f21f47abbb867a15bd66444001858
undeadsushi <sheldon.smickley@gmail.com>
2014-04-29 23:53:16 -0400

    moved js to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
-//= require_tree .
+var _bm_pageview = {
+  type: "pageview",
+  web_property_id: 2,
+  url: "http://bloccit-sheldon.herokuapp.com/",
+}
+
+var _bm_event = new XMLHttpRequest();
+_bm_event.open("POST", "http://localhost:3000/events.json", true);
+_bm_event.setRequestHeader('Content-Type', 'application/json');
+_bm_event.onreadystatechange = function() {};
+
+_bm_event.send(JSON.stringify(_bm_pageview));
